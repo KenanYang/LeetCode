d8fd64d3d168b9c2fb5f7e6e89f4a98be94b828c
D7001 <kenanyang@kenans-macbook-pro.local>
2018-03-17 15:07:40 +1100

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by KENAN YANG on 17/3/18.
+//  Copyright © 2018 KENAN YANG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
